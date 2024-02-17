880a02c654b3702e8bb020eb1f4797a14c67d02e
Tony Duran <tony-duran@outlook.com>
2024-02-16 18:21:35 -0600

    adding tactics to data output

--- data-collection/mitre/attack_mitre_data.py
+++ data-collection/mitre/attack_mitre_data.py
@@ -12,6 +12,21 @@ def fetch_attack_data():
         raise Exception(f"Failed to fetch data: {response.status_code}")
     return response.json()
 
+def extract_tactics(data):
+    tactics = []
+
+    for obj in data.get("objects", []):
+        if obj.get("type") == "x-mitre-tactic":
+            tactic_info = {
+                "ID": obj.get("external_references", [{}])[0].get("external_id"),
+                "Name": obj.get("name"),
+                "Description": obj.get("description"),
+                "URL": obj.get("external_references", [{}])[0].get("url")
+            }
+            tactics.append(tactic_info)
+
+    return tactics
+
 def extract_techniques(data):
     techniques = {}
     relationships = []
@@ -19,9 +34,11 @@ def extract_techniques(data):
     # First, collect all techniques and sub-techniques
     for obj in data.get("objects", []):
         if obj.get("type") == "attack-pattern":
+            tactics = [phase['phase_name'] for phase in obj.get('kill_chain_phases', []) if phase.get('kill_chain_name') == 'mitre-attack']
             tech_info = {
                 "ID": obj.get("external_references", [{}])[0].get("external_id"),
                 "Technique": obj.get("name"),
+                "Tactics": tactics,
                 "URL": obj.get("external_references", [{}])[0].get("url")
             }
             techniques[obj['id']] = tech_info
@@ -32,7 +49,7 @@ def extract_techniques(data):
     for rel in relationships:
         subtechnique_id = rel["source_ref"]
         parent_technique_id = rel["target_ref"]
-        
+
         if subtechnique_id in techniques and parent_technique_id in techniques:
             # Prepend parent technique name to sub-technique
             parent_name = techniques[parent_technique_id]["Technique"]
